4705cc8e6dd366429ddf2531c77d10dc06041295
Michael Chaney <mdchaney@michaelchaney.com>
2022-10-31 12:02:02 -0500

    Basic recipe works like react recipe.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'main/index'
   root 'main#index'
+
+  resources :recipes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_28_153410) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_31_145546) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_28_153410) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name", limit: 100, null: false
+    t.text "ingredients", null: false
+    t.text "instructions", null: false
+    t.string "image_url", limit: 200
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
